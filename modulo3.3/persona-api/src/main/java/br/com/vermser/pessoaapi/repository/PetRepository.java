7d500504731ec492e00760be24ebe0efcf773d9d
RodrigoSoaresNascimento <rodrigosnascimento1@gmail.com>
2022-07-28 00:47:15 -0300

    Removendo unused imports

--- modulo3.3/persona-api/src/main/java/br/com/vermser/pessoaapi/repository/PetRepository.java
+++ modulo3.3/persona-api/src/main/java/br/com/vermser/pessoaapi/repository/PetRepository.java
@@ -4,7 +4,6 @@ import br.com.vermser.pessoaapi.entity.PetEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
 
 @Repository
 public interface PetRepository extends JpaRepository<PetEntity, Integer> {
